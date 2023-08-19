a4f9dd60905a49e4b3d84c74acb88679837c3eda
Gisele Christo Santos <gsantos@cleartech.dev>
2023-08-19 18:54:19 -0300

    Sprint 12 com report

--- the-cat-api-master/demogi/test/del.spec.js
+++
@@ -1,8 +0,0 @@
-// test.js
-const { spec } = require('pactum');
-
-it('Atualizando dados', async () => {
-  await spec()
-    .delete('https://api.thecatapi.com/v1/images/search?limit=10')
-    .expectStatus(401);
-});
