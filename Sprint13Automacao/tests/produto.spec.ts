04f103bfd4c7e5b39d9e3e3003af8f3d250880f4
Gisele Christo Santos <gsantos@cleartech.dev>
2023-09-11 17:26:22 -0300

    adicionando mais testes

--- Sprint13Automacao/tests/produto.spec.ts
+++ Sprint13Automacao/tests/produto.spec.ts
@@ -23,3 +23,17 @@ for (const item of productTitleList){
     });
 
 });
+//Colocar um produto dentro do Carrinho
+
+test('colocando produto no carrinho',async ({page}) =>{
+    
+  await test.step ('login',async ()=> {
+
+      await page.goto ('https://www.saucedemo.com/');
+       await page.locator('[data-test="username"]').fill('standard_user');
+        await page.locator('[data-test="password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    });
+});
