14a90aeba4125de39f42fcc8fe4f4b22903464a2
Gisele Christo Santos <gsantos@cleartech.dev>
2023-09-11 16:57:17 -0300

    Executando mais testes no meu Desafio 13

--- Sprint13Automacao/tests/login.spec.ts
+++ Sprint13Automacao/tests/login.spec.ts
@@ -1,11 +1,32 @@
 import {   expect, test } from "@playwright/test";
 
-test('the user login with succes',async ({ page  }) => {
+test('O login do usuário feito com sucesso',async ({ page  }) => {
        await page.goto('https://www.saucedemo.com/');
         expect(await page.title()).toBe('Swag Labs');
       
         await page.locator('[data-test="username"]').fill('standard_user');
         await page.locator('[data-test="password"]').fill('secret_sauce');
+
         await page.locator('[data-test="login-button"]').click();
-        await page.pause();
+        await expect(await page.url()).toBe('https://www.saucedemo.com/inventory.html');
+
+        const productTitle = await page.locator ('.header_secondary_container > span');
+        await expect (productTitle).toHaveText('Products');
+
+
+});
+
+
+test('O login do usuário feito sem sucesso',async ({ page  }) => {
+        await page.goto('https://www.saucedemo.com/');
+
+        await page.locator('[data-test="username"]').fill('standard_user');
+        await page.locator('[data-test="password"]').fill('revelado_sauce');
+
+        await page.locator('[data-test="login-button"]').click();
+        const errorText = await page.getByText('Epic sadface: Epic sadface: Username and password do not match any user in this service');
+        //await expect(errorText).toBeVisible();
+
+
+
 });
